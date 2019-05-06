555794844180139ba24444da6aebcad0ab13bd6b
Isabella <isabellaafry@gmail.com>
2019-05-06 15:00:52 +0100

    stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const App = () => {
-    return (
-        <div>
-            Hello World
-        </div>
-    );
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            date: new Date(),
+            intervalId: undefined
+         };
+         console.log('constructor')
+    }
+    componentDidMount = () => {
+        const setDate = () => this.setState({
+            date: new Date(),
+            intervalId: interval
+        });
+        const interval = setInterval(setDate, 1000);
+        console.log('didMount')
+    }
+
+    componentDidUpdate = () => {
+        console.log('new change');
+    }
+    
+    componentWillUnmount = () => {
+        clearInterval(this.state.intervalId);
+    }
+    
+
+
+    render(){
+        console.log(this.state.intervalId);
+        return (
+            <div>
+                Hello World
+                {this.state.date.getSeconds()}
+            </div>
+        );
+    }
 };
 
 export default App;
